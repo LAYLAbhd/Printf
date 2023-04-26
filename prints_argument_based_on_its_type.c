f2060ed5dd5a3d5ebd0789bb2eb6dfaa467d8e52
Anasdarouichi <anassdarouichi@gmail.com>
2023-04-25 21:20:09 -0700

    prints_argument_based_on_its_type

--- prints_argument_based_on_its_type.c
+++ prints_argument_based_on_its_type.c
@@ -10,7 +10,7 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- * Author: ANAS DAROUICHI 
+ * Author: ANAS DAROUICHI
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
