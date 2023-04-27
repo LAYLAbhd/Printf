cb59ff02cb2148046bda30f7ae5a7c13ee81c682
LAYLAbhd <bouhad.layla@outlook.fr>
2023-04-27 13:35:49 -0700

    Calculates_the_precision_.c

--- Calculates_the_precision_printing.c
+++ Calculates_the_precision_printing.c
@@ -7,7 +7,7 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *Author : LAYLA BOUHAD
+ * Author : LAYLA BOUHAD
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
@@ -41,4 +41,3 @@ int get_precision(const char *format, int *i, va_list list)
 
 	return (precision);
 }
-
